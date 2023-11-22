793cc5fc3fa53f2976b1a284c72cc7f41a85d119
Sarthak Pati <sarthak07.dev@gmail.com>
2023-11-23 02:02:15 +0530

    new components of chat area.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="logo">We Chat</span>
+      <div className="user">
+        <img src="" alt=""></img>
+        <span>John</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
