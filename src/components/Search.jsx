3d4e4eb96ddf398d3cc25d03b2a1e49b783360cb
Sarthak Pati <sarthak07.dev@gmail.com>
2023-11-24 01:33:56 +0530

    Search component added.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a User"></input>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600"></img>
+        <div className="userChatInfo">
+          <span>Jack</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
