50e19c91c979476c73886982a9d7ea30055b252e
Sarthak Pati <sarthak07.dev@gmail.com>
2023-11-24 02:45:55 +0530

    Message interface added.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        ></img>
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
