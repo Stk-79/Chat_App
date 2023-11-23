3d85a9ec0081bc8cf079d56ff6f101db95282f07
Sarthak Pati <sarthak07.dev@gmail.com>
2023-11-24 01:35:26 +0530

    Chat profiles and styles added.

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        ></img>
+        <div className="userChatInfo">
+          <span>Jack</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        ></img>
+        <div className="userChatInfo">
+          <span>Jack</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        ></img>
+        <div className="userChatInfo">
+          <span>Jack</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
