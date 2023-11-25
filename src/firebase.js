d561d867db16ea52bad52f7d91d7b1f2e71afe98
Sarthak Pati <sarthak07.dev@gmail.com>
2023-11-26 02:08:52 +0530

    Added firebase pkg & config file.

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDwHyn4wuyyROXFUKayLz_rwr15T-ND8Ng",
+  authDomain: "chat-71d05.firebaseapp.com",
+  projectId: "chat-71d05",
+  storageBucket: "chat-71d05.appspot.com",
+  messagingSenderId: "597608971823",
+  appId: "1:597608971823:web:6e2f6946558d3c836590da",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
