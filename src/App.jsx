b1f205d91123157a527a6637409d80f6163b108e
Sarthak Pati <sarthak07.dev@gmail.com>
2023-12-08 05:03:44 +0530

    Added routing policies.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./style.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  return <Register></Register>;
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home></Home>}></Route>
+          <Route path="login" element={<Login></Login>}></Route>
+          <Route path="register" element={<Register></Register>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
