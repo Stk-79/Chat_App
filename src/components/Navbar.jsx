175c3f8d0a77e00bf9e86f5cf58bad8fd5101fa6
Sarthak Pati <sarthak07.dev@gmail.com>
2023-12-08 05:08:07 +0530

    LogOut button control added using Firebase.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <span className="logo">We Chat</span>
       <div className="user">
-        <img src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""></img>
+        <img
+          src="https://images.pexels.com/photos/1374509/pexels-photo-1374509.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        ></img>
         <span>Sarthak</span>
-        <button>logout</button>
+        <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
   );
