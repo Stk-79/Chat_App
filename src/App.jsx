ef035ef5d25dac089c43fbf2450465eea276595b
Sarthak Pati <sarthak07.dev@gmail.com>
2023-12-09 22:54:16 +0530

    Defined protected Route for Home router.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,32 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./style.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
-  console.log(currentUser);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home></Home>}></Route>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home></Home>
+              </ProtectedRoute>
+            }
+          ></Route>
           <Route path="login" element={<Login></Login>}></Route>
           <Route path="register" element={<Register></Register>}></Route>
         </Route>
